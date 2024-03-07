51cc840ee352c060868bb2c71a2d942b9f91851c
7nik <kifiranet@gmail.com>
2024-03-07 15:38:46 +0200

    chore: simplify `@connect`s

--- src/meta.user.js
+++ src/meta.user.js
@@ -6,16 +6,7 @@
 // @author       7nik, MiTereKun
 // @match        https://anime-pictures.net/*
 // @grant        GM.xmlHttpRequest
-// @connect      ip1.anime-pictures.net
-// @connect      cdn.anime-pictures.net
-// @connect      oauth.vk.com
-// @connect      login.vk.com
-// @connect      api.vk.com
-// @connect      pu.vk.com
-// @connect      id.vk.com
-// @connect      pu5-1.vk-cdn.net
+// @connect      anime-pictures.net
+// @connect      vk.com
 // @connect      vk-cdn.net
-// @connect      cpreview.anime-pictures.net
-// @connect      images.anime-pictures.net
-// @connect      cdn.anime-pictures.net
 // ==/UserScript==
